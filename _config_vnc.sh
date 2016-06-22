5c56698f8ddcc2e110ce12a4c78282f6d868f24b
Andrew Tatham <andrewtatham@gmail.com>
2016-06-22 18:53:52 +0100

    update

--- _config_vnc.sh
+++ _config_vnc.sh
@@ -1,4 +1,4 @@
+vncpasswd
 sudo cp _vncboot /etc/init.d/vncboot
 sudo chmod +x /etc/init.d/vncboot
 sudo update-rc.d vncboot defaults
-/etc/init.d/vncboot start
--- twitter.sh
+++ twitter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~/twitterpibot/
+source twitterpibot/bin/activate
 while true
 do
 	sudo pkill -f main.py
@@ -7,6 +8,8 @@ do
 	sudo git pull --rebase origin master
 	sleep 1
 	git status
+	sleep 1
+	sudo pip3 install --upgrade -r requirements.txt
 	sleep 1
 	sudo python main.py
 	sleep 1
