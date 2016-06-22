ee9f3a0d571454064dd139e99e0fc1bf137ec822
Andrew Tatham <andrewtatham@gmail.com>
2016-06-22 19:32:36 +0100

    update

--- _init_twitter.sh
+++ _init_twitter.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+sudo pip3 install virtualenv
 cd ~
 git clone https://github.com/andrewtatham/twitterpibot.git
+cd ~/twitterpibot
+virtualenv twitterpibot
+source twitterpibot/bin/activate
