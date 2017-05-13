e06d45fddaaaaf9ba841ba7fda15e7898a04792b
Andrew Tatham <andrewtatham@gmail.com>
2017-05-13 20:13:44 +0100

    scrollbot

--- _init_twitter.sh
+++ _init_twitter.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo pip3 install virtualenv
+sudo pip install sqlalchemy
+sudo pip3 install sqlalchemy
 cd ~
 git clone https://github.com/andrewtatham/twitterpibot.git
 cd ~/twitterpibot
