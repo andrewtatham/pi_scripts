d0fef9fdb020fddd753b187455a66e2e127eb2cd
Andrew Tatham <andrewtatham@gmail.com>
2017-05-13 19:24:46 +0100

    scrollbot

--- _init_packages.sh
+++ _init_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get install -y vsftpd tightvncserver autocutsel python-smbus i2c-tools
 sudo apt-get install -y python3-dev python3-setuptools
-sudo apt-get install -y libjpeg-dev libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.5-dev tk8.5-dev python-tk
+sudo apt-get install libtiff5-dev libjpeg8-dev zlib1g-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python-tk
