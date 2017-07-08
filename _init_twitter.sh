1f16792720f293125e0367d9f0f1c008e03a1407
Andrew Tatham <andrewtatham@gmail.com>
2017-07-08 11:17:06 +0100

    upgrade pip

--- _init_twitter.sh
+++ _init_twitter.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sudo pip3 install virtualenv
+sudo pip install -U pip
+sudo pip3 install -U pip
 sudo pip install apscheduler
+sudo pip3 install apscheduler
 sudo pip install facebook
+sudo pip3 install facebook
 sudo pip install sqlalchemy
 sudo pip3 install sqlalchemy
 cd ~
